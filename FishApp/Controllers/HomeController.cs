3d3a25e74c8b46e9a88dc502a6f1d60070c13dad
Ceplis Roberts <roberts.ceplis@gmail.com>
2022-12-18 16:49:14 +0200

    update

---
+++ FishApp/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using FishApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FishApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet]
+        public IActionResult Photo()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Profile()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login()
+        {
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Exit()
+        {
+            return View("login");
+        }
+    }
+}
